52f026997384e26e50b96e26f67c5ca448d6e517
Jeroen Jacobs <git@jeroenj.be>
2020-05-03 13:50:57 +0200

    [zshrc] Adds various default ENV variables

---
+++ .config/zshrc/0.main.sh
@@ -0,0 +1,9 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+fi
+
+export EDITOR="nvim"
+export VISUAL="nvim"
+
+export LANG="en_US.UTF-8"
+export LC_CTYPE=$LANG
