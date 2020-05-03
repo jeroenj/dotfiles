f8272fdec16ef2102697206626f6bd0774639c1f
Jeroen Jacobs <git@jeroenj.be>
2020-05-03 14:38:21 +0200

    [profile] Adds tmux-update-statusbar which makes thesession name red if there are multiple clients connected

--- .config/profile/tmux.sh
+++ .config/profile/tmux.sh
@@ -1,2 +1,16 @@
 alias pairstart='tmux rename pair'
 alias pairstop='tmux detach -a && tmux rename main'
+
+function tmux-update-statusbar() {
+  if [ `tmux list-clients | grep '' -c` -gt 1 ]
+  then
+    color='red'
+  else
+    color='black'
+  fi
+  tmux set -q status-left "#[fg=$color]#S:"
+}
+
+if [ -n "$TMUX" ] ; then
+  precmd_functions+=("tmux-update-statusbar")
+fi
