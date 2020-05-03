62455a41997457f8f6cbb50827f9ed357974f8d2
Jeroen Jacobs <git@jeroenj.be>
2020-05-03 15:04:20 +0200

    [profile] Sets default CHEF_ENV

--- .config/profile/chef.sh
+++ .config/profile/chef.sh
@@ -1,3 +1,5 @@
+export CHEF_ENV='personal'
+
 alias chef-cookstyle='docker run --rm -v `pwd`:/code registry.x-plose.be/xplose-docker/gitlab-pronto-chef rubocop --display-cop-names --display-style-guide -c /config/rubocop.yml'
 alias chef-foodcritic='docker run --rm -v `pwd`:/code registry.x-plose.be/xplose-docker/gitlab-pronto-chef foodcritic'
 alias pronto-cookstyle='docker run --rm -v `pwd`:/code registry.x-plose.be/xplose-docker/gitlab-pronto-chef pronto run --exit-code --formatters text -r rubocop'
