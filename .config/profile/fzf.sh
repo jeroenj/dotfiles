a59ba3ef112eb3880bdfb8235a11898c601fe103
Jeroen Jacobs <git@jeroenj.be>
2021-04-08 23:02:22 +0200

    [profile] Disables mouse integration by default in fzf

--- .config/profile/fzf.sh
+++ .config/profile/fzf.sh
@@ -1,4 +1,5 @@
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git --filename-pattern ""'
+export FZF_DEFAULT_OPTS="--no-mouse"
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   source '/usr/share/fzf/key-bindings.zsh'
