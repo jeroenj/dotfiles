79ea016d7fa0db029abdb72870411159973e9033
Jeroen Jacobs <git@jeroenj.be>
2021-06-21 21:17:41 +0200

    [profile] Sets up ssh-agent on linux

---
+++ .config/profile/ssh-agent.sh
@@ -0,0 +1,8 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent -t 12h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+  fi
+  if [[ ! "$SSH_AUTH_SOCK" ]]; then
+    source "$XDG_RUNTIME_DIR/ssh-agent.env" > /dev/null
+  fi
+fi
