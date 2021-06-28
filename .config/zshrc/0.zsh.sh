55f0e380d2b973ed727d779b3b6d2eedb8cb8068
Jeroen Jacobs <git@jeroenj.be>
2021-06-28 22:55:24 +0200

    [zshrc] Sets various shell options
    https://zsh.sourceforge.io/Doc/Release/Options.html#Options

--- .config/zshrc/0.zsh.sh
+++ .config/zshrc/0.zsh.sh
@@ -1,2 +1,12 @@
 autoload compinit && compinit
 autoload bashcompinit && bashcompinit
+
+setopt AUTO_CD
+setopt EMACS
+setopt INTERACTIVE_COMMENTS
+setopt LONG_LIST_JOBS
+
+unsetopt BG_NICE
+unsetopt CLOBBER
+unsetopt CHECK_JOBS
+unsetopt HUP
