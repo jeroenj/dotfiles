b021f00656a3bc1d0c94b2c295ccc458ecec90b7
Jeroen Jacobs <git@jeroenj.be>
2021-08-25 11:48:11 +0200

    [tmux] Replace pairstart and pairstop bash aliases by custom tmux commands

--- .config/zshrc/tmux.sh
+++ .config/zshrc/tmux.sh
@@ -1,6 +1,3 @@
-alias pairstart='tmux rename pair'
-alias pairstop='tmux detach -a && tmux rename main'
-
 function tmux-update-statusbar() {
   if [ `tmux list-clients | grep '' -c` -gt 1 ]
   then
