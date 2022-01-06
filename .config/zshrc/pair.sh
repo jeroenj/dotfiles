0f234d0a1f4bff94d1d4047d688a682521843116
Jeroen Jacobs <git@jeroenj.be>
2022-01-06 16:46:17 +0100

    [zshrc] Adds pair function which enabled/disables tmux pair sessions
    This only works on Linux.
    
    A user named `pair` is required for this to work.

---
+++ .config/zshrc/pair.sh
@@ -0,0 +1,30 @@
+function pair {
+  if [[ "$OSTYPE" != "linux-gnu" ]]; then
+    echo "Only Linux is supported." 1>&2
+    return 1
+  fi
+
+  if ! id pair &> /dev/null; then
+    echo "A user named 'pair' is required." 1>&2
+    return 1
+  fi
+
+  if [[ "$1" == "on" ]]; then
+    sudo chmod 777 /tmp/tmux-1000
+    sudo chmod 777 /tmp/tmux-1000/default
+    sudo usermod -s /usr/bin/zsh pair
+    tmux rename-session pair
+    echo "pair mode enabled"
+  elif [[ "$1" == "off" ]]; then
+    tmux rename-session main
+    tmux detach-client -a
+    sudo usermod -s /usr/sbin/nologin pair
+    # sudo killall -vu pair -HUP # TODO: iss this really needed?
+    sudo chmod 770 /tmp/tmux-1000
+    sudo chmod 770 /tmp/tmux-1000/default
+    echo "pair mode disabled"
+  else
+    echo "Provide 'on' or 'off' as argument." 1>&2
+    return 1
+  fi
+}
