597d92c342fc2e60ef1d6ed6543c995300577723
Jeroen Jacobs <git@jeroenj.be>
2022-01-12 21:14:19 +0100

    [ssh-agent] Replaces custom script by systemd service

--- .config/zshrc/ssh-agent.sh
+++ .config/zshrc/ssh-agent.sh
@@ -1,8 +1,3 @@
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent -t 12h > "$XDG_RUNTIME_DIR/ssh-agent.env"
-  fi
-  if [[ ! "$SSH_AUTH_SOCK" ]]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" > /dev/null
-  fi
+  export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent.socket"
 fi
