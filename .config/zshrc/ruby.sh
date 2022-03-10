e940b67cee66a6b42ada7296b0bb4095fb1b4fba
Jeroen Jacobs <git@jeroenj.be>
2022-03-10 21:16:38 +0100

    [zshrc] Fixes exporting gem bin path

--- .config/zshrc/ruby.sh
+++ .config/zshrc/ruby.sh
@@ -1,3 +1,3 @@
-if [[ "$OSTYPE" == "linux-gnu" && -d "$HONE/.gem/ruby" ]]; then
-  export PATH="${PATH}:$(find "$HOME/.gem/ruby/*" -maxdepth 1 -name 'bin' -type d | tail -n 1)"
+if [[ "$OSTYPE" == "linux-gnu" && -d "$HOME/.gem/ruby" ]]; then
+  export PATH="${PATH}:$(find "$HOME/.gem/ruby/"* -maxdepth 1 -name 'bin' -type d | tail -n 1)"
 fi
