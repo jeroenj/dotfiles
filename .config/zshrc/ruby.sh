b781fd84302ba18dc0cc70023cf4204f496be6c7
Jeroen Jacobs <git@jeroenj.be>
2022-03-10 21:06:18 +0100

    [zshrc] Removes ruby related aliases and functions

--- .config/zshrc/ruby.sh
+++ .config/zshrc/ruby.sh
@@ -1,20 +1,3 @@
-alias logd='tail -f log/development.log'
-alias logt='tail -f log/test.log'
-alias dbm='bin/rake db:migrate'
-alias fm='foreman start -f Procfile.dev'
-
-function sc() {
-  if [ -e bin/rails ]; then
-    ruby bin/rails console $*
-  elif [ -e script/rails ]; then
-    ruby script/rails console $*
-  elif [ -e script/console ]; then
-    ruby script/console $*
-  else;
-    echo "Not in a Rails project"
-  fi
-}
-
 if [[ "$OSTYPE" == "linux-gnu" && -d "$HONE/.gem/ruby" ]]; then
   export PATH="${PATH}:$(find "$HOME/.gem/ruby/*" -maxdepth 1 -name 'bin' -type d | tail -n 1)"
 fi
