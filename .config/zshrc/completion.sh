5ad89e152bafad953736b0bbfae5a7dd45ace561
Jeroen Jacobs <git@jeroenj.be>
2022-03-11 16:14:03 +0100

    [zshrc] Includes homebrew zsh completions on Mac

--- .config/zshrc/completion.sh
+++ .config/zshrc/completion.sh
@@ -1,3 +1,9 @@
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if type brew &>/dev/null; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+  fi
+fi
+
 autoload compinit && compinit -d ~/.cache/zsh/zcompdump
 
 if [ "~/.cache/zsh/zcompdump" -nt "~/.cache/zsh/zcompdump.zwc" -o ! -e "~/.cache/zsh/zcompdump.zwc" ]; then
