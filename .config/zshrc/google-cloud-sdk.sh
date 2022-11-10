4ef264ba928d7d7ec4f162915c3740377ab53686
Jeroen Jacobs <git@jeroenj.be>
2022-11-10 14:11:39 +0100

    [zshrc] Merge gcloud into it google-cloud-sdk file

--- .config/zshrc/google-cloud-sdk.sh
+++ .config/zshrc/google-cloud-sdk.sh
@@ -4,3 +4,5 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   source '/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 fi
+
+export CLOUDSDK_PYTHON_SITEPACKAGES=1
