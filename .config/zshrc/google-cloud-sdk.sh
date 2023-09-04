c58e6bbc7a866341b1b29a90e817875c7998fc31
Jeroen Jacobs <git@jeroenj.be>
2023-09-04 15:27:32 +0200

    [zshrc] Adds PATH for google-cloud-sdk on Mac

--- .config/zshrc/google-cloud-sdk.sh
+++ .config/zshrc/google-cloud-sdk.sh
@@ -3,6 +3,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
   source '/opt/google-cloud-cli/path.zsh.inc'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   source '/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+  source '/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
 fi
 
 export CLOUDSDK_PYTHON_SITEPACKAGES=1
