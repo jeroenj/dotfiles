d33cd7895040b463d206c03d7a75bfbbef0f0b8e
Jeroen Jacobs <git@jeroenj.be>
2024-11-20 14:23:29 +0100

    [zshrc] Sets TF_PLUGIN_CACHE_DIR for Terraform

--- .config/zshrc/terraform.sh
+++ .config/zshrc/terraform.sh
@@ -2,3 +2,4 @@ complete -C terraform terraform
 
 export TF_CLI_ARGS_plan="-parallelism=200 -lock=false"
 export TF_CLI_ARGS_apply="-parallelism=200"
+export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
